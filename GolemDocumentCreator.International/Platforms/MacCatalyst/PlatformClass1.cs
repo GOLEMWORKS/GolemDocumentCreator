45aa800300050012c1e7ad8b092cb7af5e14550e
GOLEMWORKS <korotkikh.vladimir.isr21.1@gmail.com>
2023-07-25 19:45:56 +0200

    adding international project

---
+++ GolemDocumentCreator.International/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace GolemDocumentCreator.International
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ GolemDocumentCreator.International/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GolemDocumentCreator.International
+{
+    // All the code in this file is only included on Tizen.
+    public class PlatformClass1
+    {
+    }
+}
