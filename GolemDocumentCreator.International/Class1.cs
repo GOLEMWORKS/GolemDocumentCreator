9cf0426789eed1d5754e80c43f540a82cc5bbddc
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2023-07-29 19:08:00 +0200

    Обновление зависимостей

--- GolemDocumentCreator.International/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GolemDocumentCreator.International
-{
-    // All the code in this file is included in all platforms.
-    public class Class1
-    {
-    }
-}
---
+++ GolemDocumentCreator.International/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolemDocumentCreator.International
+{
+    public class TestClass
+    {
+        public void Test(int a)
+        {
+            Console.WriteLine($"Class Test Works! {a}");
+        }
+    }
+}
